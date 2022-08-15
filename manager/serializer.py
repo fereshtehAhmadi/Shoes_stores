b4c39728591fe15b5564f4f7b7f30a18ad7dc55f
freshtehAhmadi <freshtehahmadi79@gmail.com>
2022-08-15 14:54:41 +0430

    edit permissions

--- manager/serializer.py
+++ manager/serializer.py
@@ -2,21 +2,13 @@ from rest_framework import serializers
 from manager.models import User
 
 class AdminRegisterationSerializer(serializers.ModelSerializer):
-    password2=serializers.CharField(style={'input_type':'password'}, write_only=True)
     class Meta:
         model = User
-        fields = ['username', 'name', 'phone', 'address', 'national_code', 'bank_account_number', 'password', 'password2']
+        fields = ['username', 'name', 'phone', 'address', 'national_code', 'bank_account_number', 'password']
         extra_kwargs={
             'password':{'write_only': True}
         }
     
-    def validate(self, attrs):
-        password = attrs.get('password')
-        password2 = attrs.get('password2')
-        if password != password2 :
-            raise serializers.ValidationError("password and confirm password dosen't match!!")
-        return attrs
     
     def create(self, validated_data):
-        validated_data.pop('password2')
-        return User.objects.create_user(**validated_data, is_staff=True)
+        return User.objects.create_user(**validated_data)
--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,5 @@
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from rest_framework import status, generics, views, permissions
@@ -15,12 +15,12 @@ from manager.serializer import (AdminRegisterationSerializer, )
 
 class AdminRegisterationView(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
     def post(self, request, format=None):
         serializer = AdminRegisterationSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
-            return Response({'msg': 'afareen...'}, status=status.HTTP_201_CREATED)
+            return Response({'msg': 'registeration successfully...'}, status=status.HTTP_201_CREATED)
         
         return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
