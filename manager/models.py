62d22225af133915a483f71ab8562cb6b79aef6a
freshtehAhmadi <freshtehahmadi79@gmail.com>
2022-08-15 15:00:31 +0430

    edit user model

--- manager/models.py
+++ manager/models.py
@@ -6,13 +6,13 @@ from django.core.validators import RegexValidator
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, name, phone, address, national_code, bank_account_number, is_admin, password=None, password2=None):
-        if not first_name or not last_name or not phone:
+    def create_user(self, username, name, phone, address, national_code, bank_account_number, password=None, password2=None):
+        if not name or not phone:
             raise ValueError('please fill in all fields!!')
 
         user = self.model(
             username=username,
-            name=first_name,
+            name=name,
             phone=phone,
             address=address,
             national_code=national_code,
@@ -23,21 +23,19 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, name, phone, address, national_code, bank_account_number, is_admin, password=None, password2=None):
+    def create_superuser(self, username, name, phone, address, national_code, bank_account_number, password=None, password2=None):
     
         user = self.create_user(
             username,
-            name=first_name,
+            name=name,
             password=password,
             phone=phone,
             address=address,
             national_code=national_code,
             bank_account_number=bank_account_number,
         )
-        user.is_admin = is_admin
         user.save(using=self._db)
         return user
-
 
 
 class User(AbstractBaseUser):
@@ -50,13 +48,14 @@ class User(AbstractBaseUser):
     bank_account_number = models.CharField(max_length=24)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['name', 'phone', 'address', 'national_code', 'bank_account_number', 'is_admin']
+    REQUIRED_FIELDS = ['name', 'phone', 'address', 'national_code', 'bank_account_number', ]
 
     def __str__(self):
         return self.phone
@@ -66,8 +65,3 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
-
-    @property
-    def is_staff(self):
-        return True
-
